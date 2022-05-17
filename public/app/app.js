e882de0d5ebca3eba244b751589415662f01a26d
David Seunghwan Kim (DK) <dkim@hayesco.com>
2022-05-17 11:48:07 -0500

    Removed the bad directory name publijs and files in it. Changed the `tsconfig` file in the root to `tsconfig.base`. Created another `tsconfig` file that inherits from this `base` file in the `public\app` folder. Moved the `index.html` file to the `public` folder as `webpack` now expects the `index.html` entry file to be in under `public` folder right under the root.

--- public/app/app.js
+++ public/app/app.js
@@ -1,9 +1,5 @@
-"use strict";
-// Any valid JavaScript code is a valid TypeScript code
 function startGame() {
-    // starting a new game
     var messagesElement = document.getElementById('messages');
     messagesElement.innerText = 'Welcome to MultiMath! Starting new game...';
 }
 document.getElementById('startGame').addEventListener('click', startGame);
-//# sourceMappingURL=app.js.map
--- public/app/app.ts
+++ public/app/app.ts
@@ -1,8 +1,16 @@
 // Any valid JavaScript code is a valid TypeScript code
 function startGame() {
 	// starting a new game
-	var messagesElement = document.getElementById('messages');
+
+	let playerName: string = 'Audrey';
+	logPlayer(playerName);
+
+	let messagesElement = document.getElementById('messages');
 	messagesElement!.innerText = 'Welcome to MultiMath! Starting new game...';
+}
+
+function logPlayer(name) {
+	console.log(`New game starting for player: ${name}`);
 }
 
 document.getElementById('startGame')!.addEventListener('click', startGame);
