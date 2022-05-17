a35f15994574ca16db2f01e46a3ebebe51aab51b
David Seunghwan Kim (DK) <dkim@hayesco.com>
2022-05-17 15:33:37 -0500

    Changed the entry script file for the app to point to the `public/js/app.js` and made the TS compiler to compile all TS files to `app.js` file. Also, enabled `typeAcquisition` on the TS compiler.

--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,20 @@
 module.exports = {
-  entry: './public/app/app.ts',
-  devtool: 'inline-source-map',
-  mode: 'development',
-  module: {
-    rules: [
-      {
-        test: /\.tsx?$/,
-        use: 'ts-loader',
-        exclude: /node_modules/
-      }
-    ]
-  },
-  resolve: {
-    extensions: [ '.tsx', '.ts', '.js' ]
-  },
-  output: {
-    filename: 'bundle.js'
-  }
+  entry: './public/js/app.js',
+  // devtool: 'inline-source-map',
+  // mode: 'development',
+  // module: {
+  //   rules: [
+  //     {
+  //       test: /\.tsx?$/,
+  //       use: 'ts-loader',
+  //       exclude: /node_modules/
+  //     }
+  //   ]
+  // },
+  // resolve: {
+  //   extensions: [ '.tsx', '.ts', '.js' ]
+  // },
+  // output: {
+  //   filename: 'bundle.js'
+  // }
 };
